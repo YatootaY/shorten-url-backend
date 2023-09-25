308dbf9650daa151e3603db222f7c41e9e346779
Yato <yatoakii@gmail.com>
2023-09-25 14:49:41 +0630

    feat(user): can create users

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,17 +1,16 @@
 const User = require("../models/user")
-const uuid = require("uuid")
 const asyncHandler = require("express-async-handler")
+const bcrypt = require("bcryptjs")
 
 exports.user_create = asyncHandler( async (req, res, next) => {
     if (!req.body.email || !req.body.username || !req.body.password) {
         return res.status(400).json({ message: "All fields are required" });
     }
-    const id = uuid.v4()
+    const hashPassword = bcrypt.hashSync(req.body.password,8)
     const user = new User({
-        id,
         email: req.body.email,
         username: req.body.username,
-        password: req.body.password
+        password: hashPassword
     });
     const result = await user.save();
     return res.status(201).json({ message: "User created successfully", user: result });
--- models/user.js
+++ models/user.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose")
 const Schema = mongoose.Schema
 
 const UserSchema = new Schema({
-    id: {type: String, required: true, maxLength: 100},
     email: {type: String, required: true, maxLength: 100},
     username: {type: String, required: true, maxLength: 100},
     password: {type: String, required: true}
